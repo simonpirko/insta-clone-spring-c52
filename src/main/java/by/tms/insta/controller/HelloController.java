6fed4152cb0edef559f3559bde0643add46b2272
simonpirko <s.pirko@faceclub.app>
2022-05-27 17:27:38 +0300

    Initial commit

---
+++ src/main/java/by/tms/insta/controller/HelloController.java
@@ -0,0 +1,7 @@
+package by.tms.insta.controller;
+
+/**
+ * @author Simon Pirko on 27.05.22
+ */
+public class HelloController {
+}
---
+++ src/main/java/by/tms/insta/entity/Post.java
@@ -0,0 +1,7 @@
+package by.tms.insta.entity;
+
+/**
+ * @author Simon Pirko on 27.05.22
+ */
+public class Post {
+}
