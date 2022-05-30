fc692419ecc43ce1d51738a405130b82d99c6cbb
Vadim Nahorny <spektor777@gmail.com>
2022-05-30 22:35:34 +0300

    Нагорный (аутентификация и регистрация)

--- src/main/java/by/tms/insta/entity/User.java
+++ src/main/java/by/tms/insta/entity/User.java
@@ -1,6 +1,61 @@
 package by.tms.insta.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
 public class User {
 
-    //TODO would be implemented in ICS-3
+    @NotBlank
+    @NotEmpty (message = "Имя пользователя не должно быть пустым")
+    private String login;
+
+    @NotBlank
+    @NotEmpty (message = "Пароль не должен быть пустым")
+    private String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
