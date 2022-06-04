2d72bbd43bd42d1d43a18e506443acea61bca5e4
Olga Skorik <olga.skorik17@gmail.com>
2022-06-04 09:24:44 +0300

    ics-18 commit

--- src/main/java/by/tms/insta/controller/MainController.java
+++ src/main/java/by/tms/insta/controller/MainController.java
@@ -1,7 +1,6 @@
 package by.tms.insta.controller;
 
-import by.tms.insta.dao.PostStorage;
-import by.tms.insta.dao.UserStorage;
+
 import by.tms.insta.entity.Post;
 import by.tms.insta.entity.User;
 import by.tms.insta.service.UserService;
@@ -24,15 +23,15 @@ public class MainController {
 
     @GetMapping
     public String getFollowingsPost (User user, Model model) {
-    model.addAttribute("user", user);
-    List<User> userFollowings = userService.getFollowings(user);
-    List<Post> postFollowings;
-    for (User following : userFollowings) {
-        postFollowings = postService.findPostsByUser(following);
-        for (Post post : postFollowings) {
-            model.addAttribute(post);
+        model.addAttribute("user", user);
+        List<User> userFollowings = userService.getFollowings(user);
+        List<Post> postFollowings;
+        for (User following : userFollowings) {
+            postFollowings = postService.findPostsByUser(following);
+            for (Post post : postFollowings) {
+                model.addAttribute(post);
+            }
         }
-    }
         return "index";
     }
 }
