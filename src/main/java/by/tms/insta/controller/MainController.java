134c7b1368c803d22fee71d11d6161b6c0709cd1
Olga Skorik <olga.skorik17@gmail.com>
2022-06-11 23:17:50 +0300

    ics-18 commit

--- src/main/java/by/tms/insta/controller/MainController.java
+++ src/main/java/by/tms/insta/controller/MainController.java
@@ -2,20 +2,31 @@ package by.tms.insta.controller;
 
 
 import by.tms.insta.entity.Post;
+import by.tms.insta.entity.User;
+import by.tms.insta.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PostService postService;
+
     @GetMapping
-    public String getFollowingsPost (List<Post> postList, Model model) {
-        model.addAttribute("posts", postList);
+    public String getFollowingsPost(User user, Model model) {
+//        List<User> userFollowings = userService.getFollowings(user);
+        List<Post> postFollowings = postService.findAllPosts();
+        model.addAttribute("posts", postFollowings);
         return "index";
     }
 }
